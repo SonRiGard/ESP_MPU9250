e0ab3f1036b1c6a55802c6b125caff045c9d3eb3
Rigardson01 <heozuize51@gmail.com>
2023-03-23 00:24:01 +0300

    push with library after change with gy-271

---
+++ .pio/build/nodemcu-32s/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/nodemcu-32s/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Rigardson/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Rigardson/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
---
+++ .pio/build/nodemcu-32s/src/Madgwick.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/nodemcu-32s/src/Madgwick.cpp.o: src/Madgwick.cpp \
+ src/Madgwick.h
